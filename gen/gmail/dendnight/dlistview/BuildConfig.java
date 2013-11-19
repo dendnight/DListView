5a2e7524b955c403327b649255928a0888130eb5
Dendnight <dendnight@gmail.com>
2013-11-19 11:29:51 +0800

    start project!

---
+++ gen/gmail/dendnight/dlistview/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package gmail.dendnight.dlistview;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/gmail/dendnight/dlistview/main/DListView.java
@@ -0,0 +1,27 @@
+package gmail.dendnight.dlistview.main;
+
+import android.content.Context;
+import android.widget.ListView;
+
+/**
+ * 下滑上滑阻尼
+ * 
+ * <pre>
+ * Description
+ * Author:		Dendnight
+ * Version:		1.0  
+ * Create at:	2013-11-19 上午11:22:32  
+ *  
+ * 修改历史:
+ * 日期    作者    版本  修改描述
+ * ------------------------------------------------------------------
+ * 
+ * </pre>
+ */
+public class DListView extends ListView {
+
+	public DListView(Context context) {
+		super(context);
+	}
+
+}
